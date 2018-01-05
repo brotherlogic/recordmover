cb606baac845bfdb1bc5dd99d45981f62ded5481
Simon Tucker <brotherlogic@gmail.com>
2018-01-05 08:05:30 -0800

    First version of record mover server. This closes #1.

---
+++ recordmover.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/brotherlogic/goserver"
+	"google.golang.org/grpc"
+
+	pbg "github.com/brotherlogic/goserver/proto"
+	"github.com/brotherlogic/goserver/utils"
+	pbrc "github.com/brotherlogic/recordcollection/proto"
+)
+
+//Server main server type
+type Server struct {
+	*goserver.GoServer
+	getter    getter
+	lastProc  time.Time
+	lastCount int64
+}
+
+type prodGetter struct {
+	getIP func(string) (string, int32, error)
+}
+
+func (p prodGetter) getRecords() ([]*pbrc.Record, error) {
+	ip, port, err := p.getIP("recordcollection")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := grpc.Dial(ip+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pbrc.NewRecordCollectionServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	resp, err := client.GetRecords(ctx, &pbrc.GetRecordsRequest{Filter: &pbrc.Record{}})
+	if err != nil {
+		return nil, err
+	}
+	return resp.GetRecords(), nil
+}
+
+func (p prodGetter) update(r *pbrc.Record) error {
+	ip, port, err := p.getIP("recordcollection")
+	if err != nil {
+		return err
+	}
+
+	conn, err := grpc.Dial(ip+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := pbrc.NewRecordCollectionServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err = client.UpdateRecord(ctx, &pbrc.UpdateRecordRequest{Update: r})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Init builds the server
+func Init() *Server {
+	s := &Server{GoServer: &goserver.GoServer{}}
+	s.getter = &prodGetter{getIP: utils.Resolve}
+	return s
+}
+
+// DoRegister does RPC registration
+func (s *Server) DoRegister(server *grpc.Server) {
+	// Do nothing
+}
+
+// ReportHealth alerts if we're not healthy
+func (s *Server) ReportHealth() bool {
+	return true
+}
+
+// Mote promotes/demotes this server
+func (s *Server) Mote(master bool) error {
+	return nil
+}
+
+// GetState gets the state of the server
+func (s *Server) GetState() []*pbg.State {
+	return []*pbg.State{&pbg.State{Key: "last_proc", TimeValue: s.lastProc.Unix(), Value: s.lastCount}}
+}
+
+func main() {
+	var quiet = flag.Bool("quiet", false, "Show all output")
+	flag.Parse()
+
+	//Turn off logging
+	if *quiet {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+	server := Init()
+	server.PrepServer()
+	server.Register = server
+
+	server.RegisterServer("recordmover", false)
+	server.RegisterRepeatingTask(server.moveRecords, time.Hour)
+	server.Log("Starting!")
+	server.Serve()
+}
---
+++ recordmoverutils.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	pbrc "github.com/brotherlogic/recordcollection/proto"
+)
+
+type getter interface {
+	getRecords() ([]*pbrc.Record, error)
+	update(*pbrc.Record) error
+}
+
+func (s *Server) moveRecords() {
+	t := time.Now()
+	records, err := s.getter.getRecords()
+
+	if err != nil {
+		s.Log(fmt.Sprintf("Error moving records: %v", err))
+		return
+	}
+
+	s.Log(fmt.Sprintf("About to move %v records", len(records)))
+	count := int64(0)
+	for _, record := range records {
+		s.moveRecord(record)
+		/*if update != nil {
+			count++
+			err := s.getter.update(update)
+			if err != nil {
+				s.Log(fmt.Sprintf("Error moving record: %v", err))
+			}
+		}*/
+	}
+
+	s.lastProc = time.Now()
+	s.lastCount = count
+	s.Log(fmt.Sprintf("Moved %v records (touched %v) in %v", len(records), count, time.Now().Sub(t)))
+}
+
+func (s *Server) moveRecord(r *pbrc.Record) *pbrc.Record {
+	return nil
+}

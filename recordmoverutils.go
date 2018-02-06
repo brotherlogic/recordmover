5ed5ac89709ec11911546ace8ba54cb6dac83523
Simon Tucker <brotherlogic@gmail.com>
2018-02-06 08:39:53 -0800

    Added sophmore

--- recordmoverutils.go
+++ recordmoverutils.go
@@ -67,13 +67,15 @@ func (s *Server) moveRecord(r *pbrc.Record) *pbrc.Record {
 
 	if (r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_PROFESSOR ||
 		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_POSTDOC ||
-		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_GRADUATE) && r.GetRelease().FolderId != 812802 {
+		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_GRADUATE ||
+		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PRE_SOPHMORE) && r.GetRelease().FolderId != 812802 {
 		r.GetMetadata().MoveFolder = 812802
 		return r
 	}
 	if r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_PROFESSOR ||
 		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_POSTDOC ||
-		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_GRADUATE {
+		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_GRADUATE ||
+		r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_SOPHMORE {
 		if r.GetMetadata().GetGoalFolder() != 0 && r.GetRelease().FolderId != r.GetMetadata().GetGoalFolder() {
 			r.GetMetadata().MoveFolder = r.GetMetadata().GetGoalFolder()
 			return r

86b4df57114f599d96205fd41c1ba76a772c14f9
Simon Tucker <brotherlogic@gmail.com>
2018-03-06 19:02:52 -0800

    Adds a simple API.

---
+++ recordmoverapi.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/recordmover/proto"
+)
+
+// RecordMove moves a record
+func (s *Server) RecordMove(ctx context.Context, in *pb.MoveRequest) (*pb.MoveResponse, error) {
+	in.GetMove().MoveDate = time.Now().Unix()
+	s.moves[in.GetMove().InstanceId] = in.GetMove()
+	return &pb.MoveResponse{}, nil
+}
+
+// ListMoves list the moves made
+func (s *Server) ListMoves(ctx context.Context, in *pb.ListRequest) (*pb.ListResponse, error) {
+	resp := &pb.ListResponse{Moves: make([]*pb.RecordMove, 0)}
+	for _, move := range s.moves {
+		resp.Moves = append(resp.Moves, move)
+	}
+	return resp, nil
+}
+
+// ClearMove clears a single move
+func (s *Server) ClearMove(ctx context.Context, in *pb.ClearRequest) (*pb.ClearResponse, error) {
+	delete(s.moves, in.InstanceId)
+	return &pb.ClearResponse{}, nil
+}
---
+++ recordmoverapi_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	pb "github.com/brotherlogic/recordmover/proto"
+)
+
+func InitTestServer() *Server {
+	return Init()
+}
+
+func TestRunThrough(t *testing.T) {
+	s := InitTestServer()
+
+	_, err := s.RecordMove(context.Background(), &pb.MoveRequest{Move: &pb.RecordMove{InstanceId: 1, FromFolder: 2, ToFolder: 3}})
+
+	if err != nil {
+		t.Fatalf("Error moving record: %v", err)
+	}
+
+	moves, err := s.ListMoves(context.Background(), &pb.ListRequest{})
+
+	if err != nil {
+		t.Fatalf("Error listing records: %v", err)
+	}
+
+	if len(moves.GetMoves()) != 1 || moves.GetMoves()[0].MoveDate <= 0 {
+		t.Fatalf("Move is a problem: %v", moves)
+	}
+
+	_, err = s.ClearMove(context.Background(), &pb.ClearRequest{InstanceId: 1})
+
+	if err != nil {
+		t.Fatalf("Error clearing moves: %v", err)
+	}
+
+	moves, err = s.ListMoves(context.Background(), &pb.ListRequest{})
+
+	if err != nil {
+		t.Fatalf("Error listing records: %v", err)
+	}
+
+	if len(moves.GetMoves()) != 0 {
+		t.Fatalf("Move is a problem: %v", moves)
+	}
+
+}

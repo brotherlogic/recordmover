43b77b2b4cf7fdcd5459ca41d95e0caffc28427a
Simon Tucker <brotherlogic@gmail.com>
2018-03-14 16:27:12 -0700

    Fixes the CLI

--- cli/cli.go
+++ cli/cli.go
@@ -11,11 +11,62 @@ import (
 	"github.com/brotherlogic/goserver/utils"
 	"google.golang.org/grpc"
 
+	pbgd "github.com/brotherlogic/godiscogs"
+	pbrc "github.com/brotherlogic/recordcollection/proto"
 	pb "github.com/brotherlogic/recordmover/proto"
+	pbro "github.com/brotherlogic/recordsorganiser/proto"
 
 	//Needed to pull in gzip encoding init
 	_ "google.golang.org/grpc/encoding/gzip"
 )
+
+func getRecord(instanceID int32) string {
+	host, port, err := utils.Resolve("recordcollection")
+	if err != nil {
+		log.Fatalf("Unable to reach organiser: %v", err)
+	}
+	conn, err := grpc.Dial(host+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	defer conn.Close()
+
+	if err != nil {
+		log.Fatalf("Unable to dial: %v", err)
+	}
+
+	client := pbrc.NewRecordCollectionServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	r, err := client.GetRecords(ctx, &pbrc.GetRecordsRequest{Filter: &pbrc.Record{Release: &pbgd.Release{InstanceId: instanceID}}})
+	if err != nil {
+		log.Fatalf("Unable to get records: %v", err)
+	}
+
+	return r.GetRecords()[0].GetRelease().Title
+}
+
+func getFolder(folderID int32) string {
+	host, port, err := utils.Resolve("recordsorganiser")
+	if err != nil {
+		log.Fatalf("Unable to reach organiser: %v", err)
+	}
+	conn, err := grpc.Dial(host+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	defer conn.Close()
+
+	if err != nil {
+		log.Fatalf("Unable to dial: %v", err)
+	}
+
+	client := pbro.NewOrganiserServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	r, err := client.GetQuota(ctx, &pbro.QuotaRequest{FolderId: folderID})
+	if err != nil {
+		log.Fatalf("Unable to get records: %v", err)
+	}
+
+	return r.LocationName
+}
 
 func main() {
 	host, port, err := utils.Resolve("recordmover")
@@ -40,7 +91,7 @@ func main() {
 			log.Fatalf("Error on GET: %v", err)
 		}
 		for _, move := range res.GetMoves() {
-			fmt.Printf("%v: %v -> %v\n", move.InstanceId, move.FromFolder, move.ToFolder)
+			fmt.Printf("%v: %v -> %v\n", getRecord(move.InstanceId), getFolder(move.FromFolder), getFolder(move.ToFolder))
 		}
 	case "getclear":
 		res, err := client.ListMoves(ctx, &pb.ListRequest{})
@@ -48,7 +99,7 @@ func main() {
 			log.Fatalf("Error on GET: %v", err)
 		}
 		for _, move := range res.GetMoves() {
-			fmt.Printf("%v: %v -> %v\n", move.InstanceId, move.FromFolder, move.ToFolder)
+			fmt.Printf("%v: %v -> %v\n", getRecord(move.InstanceId), move.FromFolder, move.ToFolder)
 			client.ClearMove(ctx, &pb.ClearRequest{InstanceId: move.InstanceId})
 		}
 	}

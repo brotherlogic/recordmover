1e4438124b0d7a1647876e7eae48fc43424e4d02
Simon Tucker <simon@brotherlogic@gmail>
2019-02-09 12:39:38 -0800

    Speed up refreshes

--- recordmover.go
+++ recordmover.go
@@ -289,7 +289,7 @@ func main() {
 
 	server.RegisterServer("recordmover", false)
 	server.RegisterRepeatingTask(server.moveRecords, "move_records", time.Minute)
-	server.RegisterRepeatingTask(server.refreshMoves, "refresh_moves", time.Minute)
+	server.RegisterRepeatingTask(server.refreshMoves, "refresh_moves", time.Second)
 	server.RegisterRepeatingTask(server.lookForStale, "look_for_stale", time.Minute)
 
 	fmt.Printf("%v\n", server.Serve())

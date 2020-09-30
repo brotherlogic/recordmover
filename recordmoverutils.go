3dadcea60966bf6adce844498c6f21f7805ce9f5
Simon Tucker <brotherlogic@gmail.com>
2020-09-30 10:59:27 -0700

    Also include title

--- recordmoverutils.go
+++ recordmoverutils.go
@@ -115,6 +115,7 @@ var (
 func (s *Server) moveRecordInternal(ctx context.Context, record *pbrc.Record) error {
 	folder, rule := s.moveRecord(ctx, record)
 	if record.GetRelease().GetFolderId() == 812802 && record.GetMetadata().GetRecordWidth() == 0 {
+		s.RaiseIssue(fmt.Sprintf("%v needs record width", record.GetRelease().GetInstanceId()), fmt.Sprintf("%v", record.GetRelease().GetTitle()))
 		return status.Errorf(codes.InvalidArgument, "%v needs to have the record width set", record.GetRelease().GetInstanceId())
 	}
 

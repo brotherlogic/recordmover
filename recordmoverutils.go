4f5beb08ff732bceeb8d881fd1e6743f797db39c
simon <brotherlogic@gmail.com>
2021-07-05 11:43:44 -0700

    Don't check width on records in the box

--- recordmoverutils.go
+++ recordmoverutils.go
@@ -108,8 +108,10 @@ func (s *Server) moveRecordInternal(ctx context.Context, record *pbrc.Record) er
 	if record.GetRelease().GetFolderId() == 812802 && record.GetMetadata().GetRecordWidth() == 0 &&
 		(record.GetMetadata().GetGoalFolder() != 2274270 && record.GetMetadata().GetGoalFolder() != 1782105) {
 		if folder > 0 && folder != record.GetRelease().GetFolderId() {
-			s.RaiseIssue(fmt.Sprintf("%v needs record width", record.GetRelease().GetInstanceId()), fmt.Sprintf("Record is %v and we're trying to move to %v", record.GetRelease().GetTitle(), folder))
-			return status.Errorf(codes.InvalidArgument, "%v needs to have the record width set (%v -> %v)", record.GetRelease().GetInstanceId(), record.GetRelease().GetFolderId(), folder)
+			if record.GetMetadata().GetBoxState() != pbrc.ReleaseMetadata_IN_THE_BOX {
+				s.RaiseIssue(fmt.Sprintf("%v needs record width", record.GetRelease().GetInstanceId()), fmt.Sprintf("Record is %v and we're trying to move to %v", record.GetRelease().GetTitle(), folder))
+				return status.Errorf(codes.InvalidArgument, "%v needs to have the record width set (%v -> %v)", record.GetRelease().GetInstanceId(), record.GetRelease().GetFolderId(), folder)
+			}
 		}
 	}
 

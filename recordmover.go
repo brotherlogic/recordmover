32f93f7b949f695a6904c9687b85da869c8aab0e
simon <brotherlogic@gmail.com>
2021-09-16 06:42:13 -0700

    Clears cruft

--- recordmover.go
+++ recordmover.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"sync"
 	"time"
 
@@ -263,14 +260,6 @@ func (s *Server) GetState() []*pbg.State {
 }
 
 func main() {
-	var quiet = flag.Bool("quiet", false, "Show all output")
-	flag.Parse()
-
-	//Turn off logging
-	if *quiet {
-		log.SetFlags(0)
-		log.SetOutput(ioutil.Discard)
-	}
 	server := Init()
 	server.PrepServer()
 	server.Register = server

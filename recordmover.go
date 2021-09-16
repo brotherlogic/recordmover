271f6b758002e34a33586831905dcc2110b6e790
simon <brotherlogic@gmail.com>
2021-09-16 07:01:27 -0700

    Removes state

--- recordmover.go
+++ recordmover.go
@@ -254,9 +254,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{
-		&pbg.State{Key: "blah", Value: int64(123)},
-	}
+	return []*pbg.State{}
 }
 
 func main() {
